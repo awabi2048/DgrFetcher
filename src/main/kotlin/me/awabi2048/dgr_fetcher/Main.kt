d62691ff4bbead9c528395a9c82dcc6df2f87ffe
awabi2048 <2886xl@gmail.com>
2025-02-24 10:34:25 +0900

    init

---
+++ src/main/kotlin/me/awabi2048/dgr_fetcher/Main.kt
@@ -0,0 +1,14 @@
+package me.awabi2048.dgr_fetcher
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}

c0da8f467a9a81cc3390ef591053dc0391140a9d
awabi2048 <2886xl@gmail.com>
2025-02-24 10:38:15 +0900

    init

--- src/main/kotlin/me/awabi2048/dgr_fetcher/Main.kt
+++ src/main/kotlin/me/awabi2048/dgr_fetcher/Main.kt
@@ -3,8 +3,13 @@ package me.awabi2048.dgr_fetcher
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
+    companion object {
+        lateinit var instance: JavaPlugin
+    }
 
     override fun onEnable() {
+        instance = this
+
         // Plugin startup logic
     }
 
